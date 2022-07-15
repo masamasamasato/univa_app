f2a5944d500afd7cbca2eba719c6c48b713a3f95
masato <nqa23387docomo@icloud.com>
2022-07-15 13:29:31 +0900

    postコントローラー修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_043209) do
+ActiveRecord::Schema.define(version: 2022_07_15_033552) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -69,6 +69,10 @@ ActiveRecord::Schema.define(version: 2022_03_09_043209) do
     t.string "name"
     t.text "profile"
     t.string "image"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
